f4ea5390af6922b653f7f456d27eb2e4c89c2920
chiroptical <chiroptical@gmail.com>
2025-09-27 21:20:46 -0400

    Try adding node crypto support

--- src/casper.gleam
+++ src/casper.gleam
@@ -1,7 +1,9 @@
 @external(erlang, "casper_ffi", "encrypt_internal")
+@external(javascript, "./casper_ffi.mjs", "encrypt_internal")
 fn encrypt_internal(input: BitArray, key: BitArray) -> BitArray
 
 @external(erlang, "casper_ffi", "encrypt_with_internal")
+@external(javascript, "./casper_ffi.mjs", "encrypt_with_internal")
 fn encrypt_with_internal(
   input: BitArray,
   associated_data: BitArray,
@@ -9,9 +11,11 @@ fn encrypt_with_internal(
 ) -> BitArray
 
 @external(erlang, "casper_ffi", "decrypt_internal")
+@external(javascript, "./casper_ffi.mjs", "decrypt_internal")
 fn decrypt_internal(input: BitArray, key: BitArray) -> Result(BitArray, Nil)
 
 @external(erlang, "casper_ffi", "decrypt_with_internal")
+@external(javascript, "./casper_ffi.mjs", "decrypt_with_internal")
 fn decrypt_with_internal(
   input: BitArray,
   associated_data: BitArray,
---
+++ src/casper_ffi.mjs
@@ -0,0 +1,50 @@
+import { BitArray } from "./gleam.mjs";
+import * as crypto from "node:crypto";
+
+function strongRandomBytes(n) {
+  return crypto.randomBytes(n);
+}
+                                                                            
+export function encrypt_internal(input, key) {
+  const iv = strongRandomBytes(12);
+  const cipher = crypto.createCipheriv('chacha20-poly1305', key.rawBuffer, iv);
+  let encrypted = cipher.update(input.rawBuffer);
+  encrypted += cipher.final();
+  const tag = cipher.getAuthTag();
+  return new BitArray(Buffer.concat([iv, tag, Buffer.from(encrypted)]));
+}
+
+export function encrypt_with_internal(input, associatedData, key) {
+  const iv = strongRandomBytes(12);
+  const cipher = crypto.createCipheriv('chacha20-poly1305', key.rawBuffer, iv);
+  cipher.setAAD(associatedData.rawBuffer);
+  let encrypted = cipher.update(input.rawBuffer);
+  encrypted += cipher.final();
+  const tag = cipher.getAuthTag();
+  return new BitArray(Buffer.concat([iv, tag, Buffer.from(encrypted)]));
+}
+
+export function decrypt_internal(input, key) {
+  const iv = Buffer.copyBytesFrom(input.rawBuffer, 0, 12);
+  const tag = Buffer.copyBytesFrom(input.rawBuffer, 12, 16);
+  const encrypted = Buffer.copyBytesFrom(input.rawBuffer, 28);
+  const decipher = crypto.createDecipheriv('chacha20-poly1305', key.rawBuffer, iv);
+  decipher.setAuthTag(tag);
+  let decrypted = decipher.update(encrypted);
+  console.log("begin");
+  decipher.final();
+  console.log("end");
+  return new BitArray(decrypted);
+}
+
+export function decrypt_with_internal(input, associatedData, key) {
+  const iv = Buffer.copyBytesFrom(input.rawBuffer, 0, 12);
+  const tag = Buffer.copyBytesFrom(input.rawBuffer, 12, 16);
+  const encrypted = Buffer.copyBytesFrom(input.rawBuffer, 28);
+  const decipher = crypto.createDecipheriv('chacha20-poly1305', key.rawBuffer, iv);
+  decipher.setAuthTag(tag);
+  decipher.setAAD(associatedData.rawBuffer);
+  let decrypted = decipher.update(encrypted);
+  decipher.final();
+  return new BitArray(decrypted);
+}
